4e8c7efb3b5e4af80551a4f50b0fcf1c0df42072
Augusto Pohl <augustopohl@hotmail.com>
2021-05-31 21:24:14 -0300

    Introduction text

--- src/components/Content/index.tsx
+++ src/components/Content/index.tsx
@@ -1,4 +1,4 @@
-import { Box, Flex, Heading, Text } from "@chakra-ui/layout";
+import { Box, Flex, Heading, Text, Link } from "@chakra-ui/layout";
 import React from "react";
 import Lottie from "react-lottie";
 import animationData from "../../lotties/guy-studying.json";
@@ -40,14 +40,23 @@ export default function Content() {
                         color="gray.700"
                         textAlign="justify"
                     >
-                        Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                        Ea error asperiores aut sapiente corporis excepturi rerum voluptatum quod debitis repellat sit voluptate
-                        veritatis voluptatibus quis, vitae nihil eos, fugiat iure!
+                        Se você quer aprender Inglês, você com certeza não está sozinho!<br/>
+                        <br/>
+                        De acordo com a organização <Link
+                            href="https://www.britishcouncil.cn/en/EnglishGreat/numbers"
+                            isExternal={true}
+                        >
+                            <strong>British Council</strong>
+                        </Link>, existem aproximadamente 1,5 bilhões de pessoas estudando inglês mundo a fora. E claro, com um bom motivo.
+                        <br/>
+                        Aprender a tão famosa língua universal pode potencializar não só sua carreira <strong>acadêmica</strong> ou <strong>profissional</strong>, mas também
+                        obter sucesso em conquistas pessoais tais como conhecer pessoas, <strong>viajar o mundo</strong> e conhecer as mais diversas culturas. Mas querer aprender Inglês é apenas uma parte da equação.<br/>
+                        Então, como encontrar o <strong>curso de inglês</strong> que encaixa com suas metas e expectativas e realmente faz diferença no seu aprendizado? Para responder essa pergunta, aqui vão algumas dicas fundamentais para você colocar em prática. <strong>Confira!</strong>
             </Text>
                 </Box>
 
                 <Flex
-                    my={["8", "20"]}
+                    my={["8", "160"]}
                     w={["300px", "300px", "300px", "480px"]}
                     display={["none", "none", "block"]}
                     alt="Homem segurando um livro"
@@ -55,8 +64,8 @@ export default function Content() {
                 >
                     <Lottie options={defaultOptions} height={400} width={400} />
                 </Flex>
-
             </Flex>
+            <Box w={["60px", "100px"]} mx="auto" h="1.4px" bg="gray.600" my={["1", "1"]} />
         </>
     )
 }
