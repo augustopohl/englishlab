b093ea31f0ce38d1e74f7e4929b7a7cc9aa3c58e
Augusto Pohl <augustopohl@hotmail.com>
2021-06-07 15:14:31 -0300

    prismic API call done

--- src/components/Content/ContentTips.tsx
+++ src/components/Content/ContentTips.tsx
@@ -1,73 +1,55 @@
-import { Box, Flex } from "@chakra-ui/layout";
+import { Box, Flex, Text } from "@chakra-ui/layout";
 import React from "react";
-import { HomepageProps } from "../../pages";
-import TipsChallenge from "./TipsChallenge";
+import { TipsProps } from "../../pages";
 import TipsContent from "./TipsContent";
 import TipsTitle from "./TipsTitle";
 
-export default function ContentTips({ homepageTips }: HomepageProps) {
+export default function ContentTips({ tips }: TipsProps) {
     return (
         <>
-            <Flex>
-                <Box
-                    maxWidth="1160px"
-                    mx="auto"
-                    mb="10"
-                    px={["5", "3"]}
-                    my={["8", "30"]}
-                >
-                    <Flex
-                        my={["50", "100"]}
-                        w="100%"
-                        h={["163px", "250px", "350px", "535px"]}
-                        bgImage="url('/goals.jpg')"
-                        bgPosition={["100% 20%", "100% 20%", "100% 10%"]}
-                        bgRepeat="no-repeat"
-                        bgSize="cover"
-                        boxShadow="1px 2px 2px rgba(0, 0, 0, 0.5)"
-                    />
-                    <TipsTitle>Entenda o seu objetivo 🎯</TipsTitle>
-                    <TipsContent>
-                        Muitas pessoas desejam aprender inglês para poder se comunicar com pessoas ao redor do mundo. Mas existem formas variadas de adquirir a compreensão desse idioma, de acordo com seus objetivos.
-                        
-                        As línguas são compostas por quatro habilidades: a fala, a escrita, a leitura e a audição. Em geral, os cursos de idiomas investem em metodologias que trabalham todas essas habilidades ao mesmo tempo, abrangendo um pouco de cada uma durante as aulas.
-
-                        Porém, se você tem uma meta específica — leitura e escrita, já que precisa fazer uma prova ou concurso, por exemplo —, vale a pena procurar por aulas direcionadas para um propósito. Elas  serão mais objetivas e práticas, te ajudando a ganhar tempo e chegar exatamente onde você quer.
-
-                        O inglês instrumental, por exemplo, é uma técnica de ensino pensada para alunos que precisam lidar com leitura de livros e artigos em inglês. Caso seu objetivo seja uma viagem, é possível fazer aulas que foquem em conversação, para se preparar para situações específicas, como passar pelo aeroporto, ir a um restaurante, fazer check-in no hotel, entre outros.
-                </TipsContent>
-                <TipsChallenge />
-                </Box>
-            </Flex>
-            <Flex>
-                <Box
-                    maxWidth="1160px"
-                    mx="auto"
-                    mb="10"
-                    px={["5", "3"]}
-                    my={["8", "30"]}
-                >
-                    <Flex
-                        my={["50", "100"]}
-                        w="100%"
-                        h={["163px", "250px", "350px", "535px"]}
-                        bgImage="url('/methodology.jpg')"
-                        bgPosition={["100% 20%", "100% 20%", "100% 10%"]}
-                        bgRepeat="no-repeat"
-                        bgSize="cover"
-                        boxShadow="1px 2px 2px rgba(0, 0, 0, 0.5)"
-                    />
-                    <TipsTitle>Busque conhecer a metodologia 📚</TipsTitle>
-                    <TipsContent>
-                    É importante lembrar que estudar em uma escola de idiomas é um investimento tanto de tempo quanto de dinheiro. Então, é preciso avaliar bem cada escola antes de tomar sua decisão.
-
-                    Desconfie de escolas que apresentam slogans que garantem o aprendizado em tempo recorde. Como dissemos, leva algum tempo para aprender uma língua nova. Se o estabelecimento prometer que em três meses você irá do iniciante ao fluente, talvez essa não seja a melhor escola de inglês para você, porque eles não estão sendo sinceros.
-
-                    Visite as escolas que você mais gosta e pergunte se pode assistir uma aula gratuita para avaliar os professores. Assim, será possível ver quantos alunos tem por classe, quais os materiais e método de ensino utilizado. Também poderá ver a didática do professor e outros elementos que serão essenciais na hora do seu aprendizado.
-                    </TipsContent>
-                <TipsChallenge />
-                </Box>
-            </Flex>
+            {tips.map(tip => (
+                <Flex>
+                    <Box
+                        maxWidth="1160px"
+                        mx="auto"
+                        mb="10"
+                        px={["5", "3"]}
+                        my={["8", "30"]}
+                    >
+                        <Flex
+                            my={["50", "100"]}
+                            w="100%"
+                            h={["163px", "250px", "350px", "535px"]}
+                            bgImage={`url(${tip.image})`}
+                            bgPosition={["100% 20%", "100% 20%", "100% 10%"]}
+                            bgRepeat="no-repeat"
+                            bgSize="cover"
+                            boxShadow="1px 2px 2px rgba(0, 0, 0, 0.5)"
+                        />
+                        <TipsTitle>{tip.title}</TipsTitle>
+                        <TipsContent>
+                            {tip.text}
+                        </TipsContent>
+                        <Flex flexDirection="column">
+                            <Text
+                                color="orange.400"
+                                fontWeight="bold"
+                                fontSize={["md",
+                                    "md",
+                                    "xl"]}
+                            >
+                                {tip.challenge_title}
+                            </Text>
+                            <Text
+                                color="gray.700"
+                                fontSize={["sm", "md", "md", "l"]}
+                            >
+                                {tip.challenge_text}
+                            </Text>
+                        </Flex>
+                    </Box>
+                </Flex>
+            ))}
         </>
     )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,28 +9,26 @@ import getPrismicClient from "../services/prismic";
 import Prismic from '@prismicio/client';
 import { RichText } from 'prismic-dom'
 
-type Section = {
-  section: {
-    image: string,
-    title: string,
-    text: string,
-    challenge_title: string,
-    challenge_text: string,
-  }[]
+type Tip = {
+  image: string,
+  title: string,
+  text: string,
+  challenge_title: string,
+  challenge_text: string,
 }
 
-export interface HomepageProps {
-  homepageTips: Section[]
+export interface TipsProps {
+  tips: Tip[];
 }
 
-export default function Home({ homepageTips }: HomepageProps) {
+export default function Home({ tips }: TipsProps) {
   return (
     <>
       <Header />
       <Banner />
       <Content />
       <Divider />
-      <ContentTips homepageTips={homepageTips} />
+      <ContentTips tips={tips} />
       <Footer />
     </>
   )
@@ -41,27 +39,25 @@ export const getStaticProps: GetStaticProps = async () => {
 
   const response = await prismic.query(
     [Prismic.Predicates.at('document.type', 'homepage')]
-  )
+  );
 
-  const homepageTips = response.results.map(homepageTip => {
+  const homepageTips = response.results[0].data;
+
+  const tips = homepageTips.section.map(tip => {
     return {
-      section: homepageTip.data.section.map(tip => {
-        return {
-          image: tip.image.url,
-          title: RichText.asText(tip.title),
-          text: RichText.asText(tip.text),
-          challenge_title: RichText.asText(tip.challenge_title),
-          challenge_text: RichText.asText(tip.challenge_text),
-        }
-      })
-    }
-  })
+      image: tip.image.url,
+      title: RichText.asText(tip.title),
+      text: RichText.asText(tip.text),
+      challenge_title: RichText.asText(tip.challenge_title),
+      challenge_text: RichText.asText(tip.challenge_text),
+    };
+  });
 
-  console.log(JSON.stringify(homepageTips, null, 2))
+  console.log(JSON.stringify(tips, null, 2))
 
   return {
     props: {
-      homepageTips
+      tips
     },
     revalidate: 1800,
   }
